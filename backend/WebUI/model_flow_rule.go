2d8ab7dea94eb385eea1b5fdb2bc75693c607c9d
JJShen2000 <gimgim7910@gmail.com>
2022-12-14 14:22:37 +0800

    refactor: Change flowrule struct tag

--- backend/WebUI/model_flow_rule.go
+++ backend/WebUI/model_flow_rule.go
@@ -1,7 +1,7 @@
 package WebUI
 
 type FlowRule struct {
-	Filter       string       `json:"filter,omitempty" yaml:"filter" bson:"filter" mapstructure:"filter"`
+	Filter       string       `json:"filter" yaml:"filter" bson:"filter" mapstructure:"filter"`
 	Snssai       string       `json:"snssai,omitempty" yaml:"snssai" bson:"snssai" mapstructure:"snssai"`
 	Dnn          string       `json:"dnn,omitempty" yaml:"v" bson:"dnn" mapstructure:"dnn"`
 	Var5QI       int          `json:"5qi,omitempty" yaml:"5qi" bson:"5qi" mapstructure:"5qi"`
@@ -11,6 +11,6 @@ type FlowRule struct {
 	GBRDL        string       `json:"gbrDL,omitempty" yaml:"gbrDL" bson:"gbrDL" mapstructure:"gbrDL"`
 	ChargingData ChargingData `json:"charging,omitempty" yaml:"charging" bson:"charging" mapstructure:"charging"`
 	RatingGroup  int32        `json:"ratingGroup,omitempty" yaml:"ratingGroup" bson:"ratingGroup" mapstructure:"ratingGroup"`
-	Online       bool         `json:"onlineChargingChk,omitempty" yaml:"Online" bson:"Online" mapstructure:"Online"`
-	Offline      bool         `json:"offlineChargingChk,omitempty" yaml:"Offline" bson:"Offline" mapstructure:"Offline"`
+	Online       bool         `json:"onlineChargingChk" yaml:"Online" bson:"Online" mapstructure:"Online"`
+	Offline      bool         `json:"offlineChargingChk" yaml:"Offline" bson:"Offline" mapstructure:"Offline"`
 }
